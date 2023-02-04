5c248541e3ce934b2bfdb1d019eeaddbe2c0d531
João Santos <joaosantosdev97@gmail.com>
2023-02-03 23:19:35 -0300

    Socials

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,6 +2,8 @@ import twitter from "../../Assets/Social/twitter.png";
 import medium from "../../Assets/Social/medium.png";
 import discord from "../../Assets/Social/discord.png";
 import github from "../../Assets/Social/github.png";
+import telegram from "../../Assets/Social/telegram.png";
+import youtuber from "../../Assets/Social/youtube.png";
 
 const Contact = () => {
   return (
@@ -11,7 +13,7 @@ const Contact = () => {
     >
       <div className="flex flex-col justify-center items-center">
         <h2 className="py-4 text-3xl font-bold text-center">Stay tuned</h2>
-        <div className="flex justify-center flex-wrap max-w-2xl">
+        <div className="flex justify-center flex-wrap max-w-5xl">
           <div className="p-4 flex flex-col gap-7 border w-[300px] justify-center items-center">
             <div className="flex justify-center items-center lg:col-span-1 col-span-full">
               <img src={github} alt="github" className="h-20" />
@@ -59,6 +61,58 @@ const Contact = () => {
                 rel="noreferrer"
               >
                 <img src={twitter} alt="github" className="h-20" />
+              </a>
+            </div>
+            <div className="flex flex-col items-center justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+              <span className="text-xs tracking-wider uppercase dark:text-violet-400">
+                Trending topics
+              </span>
+              <span className="text-xl font-bold md:text-2xl">
+                <a
+                  href="https://twitter.com/KrstmTech"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  Twitter updates
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="p-4 flex flex-col gap-7 border w-[300px] justify-center items-center">
+            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+              <a
+                href="https://twitter.com/KrstmTech"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={youtuber} alt="github" className="h-20" />
+              </a>
+            </div>
+            <div className="flex flex-col items-center justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
+              <span className="text-xs tracking-wider uppercase dark:text-violet-400">
+                Trending topics
+              </span>
+              <span className="text-xl font-bold md:text-2xl">
+                <a
+                  href="https://twitter.com/KrstmTech"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cursor-pointer"
+                >
+                  Twitter updates
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="p-4 flex flex-col gap-7 border w-[300px] justify-center items-center">
+            <div className="flex items-center justify-center lg:col-span-1 col-span-full">
+              <a
+                href="https://twitter.com/KrstmTech"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={telegram} alt="github" className="h-20" />
               </a>
             </div>
             <div className="flex flex-col items-center justify-center max-w-3xl text-center col-span-full lg:col-span-3 lg:text-left">
